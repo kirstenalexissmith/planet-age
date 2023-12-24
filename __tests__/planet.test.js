7aea13d3e4a581f1c267a579d73c288abddc6eda
Kirsten Smith <kirstenalexis7@gmail.com>
2023-12-24 14:57:51 -0800

    mercuryAge function test good fail

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -1,9 +1,17 @@
 import PlanetAge from '../src/planet.js';
 
 describe('PlanetAge', () => {
+  let kirsten;
+
+  beforeEach(() => {
+    kirsten = new PlanetAge(26);
+  })
 
   test('should create class object for planet ages', () => {
-    const kirsten = new PlanetAge(26);
     expect(kirsten.earthAge).toBe(26);
   });
+
+  test('should calculate users age in mercury years', () => {
+    expect(kirsten.mercuryAge()).toBe(108)
+  })
 })
--- src/planet.js
+++ src/planet.js
@@ -2,4 +2,8 @@ export default class PlanetAge {
   constructor(earthAge) {
     this.earthAge = earthAge;
   }
+
+  mercuryAge() {
+
+  }
 }
