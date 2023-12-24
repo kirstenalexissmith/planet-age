963d7b30097758f2d91d7b4c205b3bc3067a048a
Kirsten Smith <kirstenalexis7@gmail.com>
2023-12-24 14:33:12 -0800

    PlanetAge test 1 pass

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -5,6 +5,5 @@ describe('PlanetAge', () => {
   test('should create class object for planet ages', () => {
     const kirsten = new PlanetAge(26);
     expect(kirsten.earthAge).toBe(26);
-  })
-
+  });
 })
